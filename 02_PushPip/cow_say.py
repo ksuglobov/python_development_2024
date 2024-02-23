4daa004cf1a955d0e78338f111ff62c20e5005cb
ksuglobov <ksuglobov@outlook.com>
2024-02-23 03:11:55 +0300

    Add cow args

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,4 +1,4 @@
-import cowsay
+from cowsay import cowsay, Option as default_cow,
 import argparse
 
 
@@ -25,6 +25,40 @@ def main():
         default=40,
         help='Width to wrap the message (default: %(default)s). Works if wrapping is enabled.',
         dest='width'
+    )
+
+    # cow parameters
+    parser.add_argument('-f',
+        type=str,
+        default="default",
+        help='Either the name of a cow specified in the COWPATH (default: %(default)s) or path to cowfile.',
+        dest='cow'
+    )
+
+    cow_mode_parser = parser.add_argument_group(
+        title='Cow mode',
+        description='Affects the appearance of the cow. If multiple modes are given, the one furthest down in this list is selected'
+    )
+    cow_mode_parser.add_argument('-b', action='store_const', const='b', help='Borg')
+    cow_mode_parser.add_argument('-d', action='store_const', const='d', help='dead')
+    cow_mode_parser.add_argument('-g', action='store_const', const='g', help='greedy')
+    cow_mode_parser.add_argument('-p', action='store_const', const='p', help='paranoid')
+    cow_mode_parser.add_argument('-s', action='store_const', const='s', help='stoned')
+    cow_mode_parser.add_argument('-t', action='store_const', const='t', help='tired')
+    cow_mode_parser.add_argument('-w', action='store_const', const='w', help='wired')
+    cow_mode_parser.add_argument('-y', action='store_const', const='y', help='young')
+
+    parser.add_argument('-e',
+        type=str,
+        default=default_cow.eyes,
+        help='Cow\'s eyes string (default: %(default)s). Works if no cow mode specified.',
+        dest='eyes'
+    )
+    parser.add_argument('-T',
+        type=str,
+        default=default_cow.tongue,
+        help='Cow\'s tongue string (default: %(default)s). Works if no cow mode specified.',
+        dest='tongue'
     )
 
 
