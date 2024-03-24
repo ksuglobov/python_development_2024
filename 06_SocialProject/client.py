4bde5989cedd949805db79ff1acc9f92bf452c5c
ksuglobov <ksuglobov@outlook.com>
2024-03-24 20:58:25 +0300

    Add basic client

---
+++ 06_SocialProject/client.py
@@ -0,0 +1,32 @@
+import cmd
+import sys
+import socket
+import threading
+import readline
+
+
+class CowClient(cmd.Cmd):
+
+    def __init__(self, socket):
+        super().__init__()
+        self.prompt = '>>> '
+        self.socket = socket
+        self.is_running = True
+
+    def do_send(self, args):
+        self.socket.sendall(f'{args}\n'.encode())
+
+    def receive(self):
+        while self.is_running:
+            res = s.recv(1024).rstrip().decode()
+            print(f'\n{res}\n{self.prompt}{readline.get_line_buffer()}', end='', flush=True) 
+
+
+host = 'localhost' if len(sys.argv) < 2 else sys.argv[1]
+port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+    cow_client = CowClient(s)
+    receiver = threading.Thread(target=cow_client.receive)
+    receiver.start()
+    cow_client.cmdloop()
