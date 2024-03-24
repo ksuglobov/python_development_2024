15a055e57c6fba038f23910717e72ac55d639d50
ksuglobov <ksuglobov@outlook.com>
2024-03-24 22:32:37 +0300

    Add login logic

--- 06_SocialProject/client.py
+++ 06_SocialProject/client.py
@@ -19,6 +19,9 @@ class CowClient(cmd.Cmd):
     def do_cows(self, args):
         self.socket.sendall(f'cows\n'.encode())
 
+    def do_login(self, args):
+        self.socket.sendall(f'login {args}\n'.encode())
+
     def do_quit(self, args):
         self.socket.sendall(f'quit\n'.encode())
         self.is_running = False
